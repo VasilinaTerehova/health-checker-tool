744d5d2fe19acc24eb0820ea96c84f933015131b
Vasilina Terehova <vasilina_terehova@epam.com>
2018-04-02 19:03:14 +0300

    cluster status model

---
+++ facade/model/src/main/java/com/epam/facade/model/ClusterStatus.java
@@ -0,0 +1,10 @@
+package com.epam.facade.model;
+
+/**
+ * Created by Vasilina_Terehova on 3/30/2018.
+ */
+public class ClusterStatus {
+    int usedMemory;
+    int countOfRunningApplications;
+    double percentageOfDiskUsage;
+}
